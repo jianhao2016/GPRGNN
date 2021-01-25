a174a92d140b628312bb94b4637c5b706152ac45
jianhao <jianhao2@illinois.edu>
2021-01-25 11:43:30 -0800

    jianhao: added dataset chameleon, film, and squirrel

--- src/dataset_utils.py
+++ src/dataset_utils.py
@@ -185,21 +185,21 @@ def DataLoader(name):
     #                 'chameleon', 'film', 'squirrel',
     #                 'Texas', 'Cornell']
 
-    if name in ['cSBM_data_Aug_19_2020-13:06',
-                'cSBM_data_Aug_18_2020-18:50',
-                'cSBM_data_Aug_21_2020-10:06',
-                'cSBM_data_Aug_19_2020-20:41',
-                'cSBM_data_Aug_21_2020-11:04',
-                'cSBM_data_Aug_21_2020-11:21',
-                'cSBM_data_Sep_01_2020-14:15',
-                'cSBM_data_Sep_01_2020-14:18',
-                'cSBM_data_Sep_01_2020-14:19',
-                'cSBM_data_Sep_01_2020-14:32',
-                'cSBM_data_Sep_01_2020-14:22',
-                'cSBM_data_Sep_01_2020-14:23',
-                'cSBM_data_Sep_01_2020-14:27',
-                'cSBM_data_Sep_01_2020-14:29']:
-
+    # if name in ['cSBM_data_Aug_19_2020-13:06',
+    #             'cSBM_data_Aug_18_2020-18:50',
+    #             'cSBM_data_Aug_21_2020-10:06',
+    #             'cSBM_data_Aug_19_2020-20:41',
+    #             'cSBM_data_Aug_21_2020-11:04',
+    #             'cSBM_data_Aug_21_2020-11:21',
+    #             'cSBM_data_Sep_01_2020-14:15',
+    #             'cSBM_data_Sep_01_2020-14:18',
+    #             'cSBM_data_Sep_01_2020-14:19',
+    #             'cSBM_data_Sep_01_2020-14:32',
+    #             'cSBM_data_Sep_01_2020-14:22',
+    #             'cSBM_data_Sep_01_2020-14:23',
+    #             'cSBM_data_Sep_01_2020-14:27',
+    #             'cSBM_data_Sep_01_2020-14:29']:
+    if 'cSBM_data' in name:
         path = '../data/'
         dataset = dataset_ContextualSBM(path, name=name)
     else:
