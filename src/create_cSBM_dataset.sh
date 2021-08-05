f4aaad6ca28c83d3121338a4c4fe5d162edfa9a2
Jianhao <pengjianhao123@outlook.com>
2021-08-05 11:43:58 -0500

    Update create_cSBM_dataset.sh

--- src/create_cSBM_dataset.sh
+++ src/create_cSBM_dataset.sh
@@ -5,7 +5,7 @@
 python cSBM_dataset.py --phi 0.5 \
     --name cSBM_phi_0.5 \
     --root ../data/ \
-    --num_nodes 800 \
-    --num_features 1000 \
+    --num_nodes 5000 \
+    --num_features 2000 \
     --avg_degree 5 \
     --epsilon 3.25 
