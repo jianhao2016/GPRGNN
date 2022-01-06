dc246833865af87ae5d4e965d189608f8832ddac
Eli Chien <46387845+elichienxD@users.noreply.github.com>
2022-01-06 14:15:21 +0800

    Update Reproduce_GPRGNN.sh

--- src/Reproduce_GPRGNN.sh
+++ src/Reproduce_GPRGNN.sh
@@ -47,50 +47,50 @@ python train_model.py --RPMAX $RPMAX \
         --alpha 0.5 \
         --weight_decay 0.0
 
-# # Below is for heterophily datasets, dense split
+# Below is for heterophily datasets, dense split
 
-# python train_model.py --RPMAX $RPMAX \
-#         --net GPRGNN \
-#         --train_rate 0.6 \
-#         --val_rate 0.2 \
-#         --dataset chameleon \
-#         --lr 0.05 \
-#         --alpha 1.0 \
-#         --weight_decay 0.0 \
-#         --dprate 0.7 
+python train_model.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset chameleon \
+        --lr 0.05 \
+        --alpha 1.0 \
+        --weight_decay 0.0 \
+        --dprate 0.7 
 
-# python train_model.py --RPMAX $RPMAX \
-#         --net GPRGNN \
-#         --train_rate 0.6 \
-#         --val_rate 0.2 \
-#         --dataset film \
-#         --lr 0.01 \
-#         --alpha 0.9 \
-#         --weight_decay 0.0 
+python train_model.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset film \
+        --lr 0.01 \
+        --alpha 0.9 \
+        --weight_decay 0.0 
 
-# python train_model.py --RPMAX $RPMAX \
-#         --net GPRGNN \
-#         --train_rate 0.6 \
-#         --val_rate 0.2 \
-#         --dataset squirrel \
-#         --lr 0.05 \
-#         --alpha 0.0 \
-#         --weight_decay 0.0 \
-#         --dprate 0.7 
+python train_model.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset squirrel \
+        --lr 0.05 \
+        --alpha 0.0 \
+        --weight_decay 0.0 \
+        --dprate 0.7 
         
-# python train_model.py --RPMAX $RPMAX \
-#         --net GPRGNN \
-#         --train_rate 0.6 \
-#         --val_rate 0.2 \
-#         --dataset texas \
-#         --lr 0.05 \
-#         --alpha 1.0 \
+python train_model.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset texas \
+        --lr 0.05 \
+        --alpha 1.0 \
         
-# python train_model.py --RPMAX $RPMAX \
-#         --net GPRGNN \
-#         --train_rate 0.6 \
-#         --val_rate 0.2 \
-#         --dataset cornell \
-#         --lr 0.05 \
-#         --alpha 0.9 
+python train_model.py --RPMAX $RPMAX \
+        --net GPRGNN \
+        --train_rate 0.6 \
+        --val_rate 0.2 \
+        --dataset cornell \
+        --lr 0.05 \
+        --alpha 0.9 
 
